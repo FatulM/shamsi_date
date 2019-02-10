2d367652e796d4cd8973471d6c6062178a84e313
FatulM <madani_amirreza@yahoo.com>
2019-02-10 20:19:21 +0330

    ADDED gregorian formatter

--- lib/shamsi_date.dart
+++ lib/shamsi_date.dart
@@ -1,5 +1,8 @@
 library shamsi_date;
 
 export 'package:shamsi_date/src/date.dart';
+export 'package:shamsi_date/src/date_formatter.dart';
 export 'package:shamsi_date/src/gregorian/gregorian_date.dart';
+export 'package:shamsi_date/src/gregorian/gregorian_formatter.dart';
 export 'package:shamsi_date/src/jalali/jalali_date.dart';
+export 'package:shamsi_date/src/jalali/jalali_formatter.dart';
---
+++ lib/src/gregorian/gregorian_formatter.dart
@@ -0,0 +1,43 @@
+library gregorian_formatter;
+
+import 'package:shamsi_date/src/date_formatter.dart';
+import 'package:shamsi_date/src/gregorian/gregorian_date.dart';
+
+class GregorianFormatter extends DateFormatter<Gregorian> {
+  GregorianFormatter(Gregorian date) : super(date);
+
+  static const List<String> _monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  static const List<String> _weekDayNames = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
+
+  @override
+
+  /// Gregorian month name
+  String get mN => _monthNames[date.month - 1];
+
+  @override
+
+  /// Gregorian week day name
+  String get wN => _weekDayNames[date.month - 1];
+}
