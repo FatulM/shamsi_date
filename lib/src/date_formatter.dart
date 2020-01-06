0677cbe692a0bb3adf81673c826136b7eac51816
FatulM <madani_amirreza@yahoo.com>
2020-01-06 19:43:07 +0330

    IMPROVED date formatter performance

--- lib/src/date_formatter.dart
+++ lib/src/date_formatter.dart
@@ -19,19 +19,19 @@ abstract class DateFormatter {
   /// year number string whatever length it has
   /// [date.year] should be greater or equal 0 or exception will be thrown
   String get y {
-    final year = date.year;
+    final int year = date.year;
 
     if (year < 0) {
       throw StateError('date.year = $year < 0');
     }
 
-    return '$year';
+    return year.toString();
   }
 
   /// year number string ensured to have length of 4
   /// [date.year] should be between 0 and 9999 or exception will be thrown
   String get yyyy {
-    final year = date.year;
+    final int year = date.year;
 
     if (year < 0) {
       throw StateError('date.year = $year < 0');
@@ -41,17 +41,17 @@ abstract class DateFormatter {
       throw StateError('date.year = $year > 9999');
     }
 
-    final str = '$year';
+    final String str = year.toString();
 
     switch (str.length) {
       case 4:
         return str;
       case 3:
-        return '0$str';
+        return '0' + str;
       case 2:
-        return '00$str';
+        return '00' + str;
       case 1:
-        return '000$str';
+        return '000' + str;
       default: // case: 0
         return '0000';
     }
@@ -60,7 +60,7 @@ abstract class DateFormatter {
   /// year number string ensured to have length of 2
   /// [date.year] should be between 1000 and 9999 or exception is thrown
   String get yy {
-    final year = date.year;
+    final int year = date.year;
 
     if (year < 1000) {
       throw StateError("date.year = $year < 1000");
@@ -70,18 +70,18 @@ abstract class DateFormatter {
       throw StateError("date.year = $year > 9999");
     }
 
-    final str = '${year % 100}';
+    final String str = (year % 100).toString();
     return str.length == 1 ? '0' + str : str;
   }
 
   /// month number string whatever length it has
   String get m {
-    return '${date.month}';
+    return date.month.toString();
   }
 
   /// month number string ensured to have length of 2
   String get mm {
-    final str = m;
+    final String str = m;
     return str.length == 1 ? '0' + str : str;
   }
 
@@ -90,12 +90,12 @@ abstract class DateFormatter {
 
   /// day number string whatever length it has
   String get d {
-    return '${date.day}';
+    return date.day.toString();
   }
 
   /// day number string ensured to have length of 2
   String get dd {
-    final str = d;
+    final String str = d;
     return str.length == 1 ? '0' + str : str;
   }
 
