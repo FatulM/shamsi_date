5916c5025979f217a85519344e12ce6d9cb668c2
FatulM <madani_amirreza@yahoo.com>
2020-01-23 21:58:27 +0330

    FIX dart docs

--- test/shamsi_date_test.dart
+++ test/shamsi_date_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2018 - 2020, Amirreza Madani. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shamsi_date/shamsi_date.dart';
 
