114c56c97c4e134be703bc4736cd970814ed8aae
FatulM <madani_amirreza@yahoo.com>
2020-12-18 22:34:46 +0330

    nullsafety ADDED example project

---
+++ example/lib/main.dart
@@ -0,0 +1,60 @@
+// Copyright 2018 - 2020, Amirreza Madani. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:shamsi_date/shamsi_date.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'shamsi_date example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  String _format(Date date) {
+    final f = date.formatter;
+
+    return '${f.wN} ${f.d} ${f.mN} ${f.yy}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('shamsi_date example'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                _format(Gregorian.now()),
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              Text(
+                _format(Jalali.now()),
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
