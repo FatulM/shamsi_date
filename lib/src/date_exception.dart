4ad42ee1385478c82989cab458c70f8e12519e45
FatulM <madani_amirreza@yahoo.com>
2021-02-06 17:48:46 +0330

    UPDATED LICENSE

--- lib/src/date_exception.dart
+++ lib/src/date_exception.dart
@@ -1,4 +1,4 @@
-// Copyright 2018 - 2020, Amirreza Madani. All rights reserved.
+// Copyright 2018 - 2021, Amirreza Madani. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
