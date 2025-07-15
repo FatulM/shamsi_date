270037b95f019bc3fc81ce2d729d5c89f55bd6ca
Ashkan <87552247+AshkanWatson@users.noreply.github.com>
2025-07-15 06:52:32 +0330

    Add Finglish month names jalali formatter (#29)
    * Add Finglish month names jalali formatter
    
    I needed the month names in Finglish for my personal project, and i thought it might be useful for others as well.
    so i added a simple  constant list of month names in Finglish as monthNamesFinglish , and a simple getter mNF to access the current date's month name in Finglish
    
    Let me know if you have any feedback or suggestion!
    
    * Adding mNFn , wNFn test

--- lib/src/jalali/jalali_formatter.dart
+++ lib/src/jalali/jalali_formatter.dart
@@ -26,6 +26,22 @@ class JalaliFormatter extends DateFormatter {
     'اسفند',
   ];
 
+  /// Jalali month names in Finglish
+  static const List<String> _monthNamesFinglish = [
+    'Farvardin',
+    'Ordibehesht',
+    'Khordad',
+    'Tir',
+    'Mordad',
+    'Shahrivar',
+    'Mehr',
+    'Aban',
+    'Azar',
+    'Dey',
+    'Bahman',
+    'Esfand',
+  ];
+
   /// Jalali Month Names in Afghanistan
   static const List<String> _monthNamesAfghanistan = [
     'حمل',
@@ -53,10 +69,26 @@ class JalaliFormatter extends DateFormatter {
     'جمعه',
   ];
 
+  /// Jalali week day names in Finglish
+  static const List<String> _weekDayNamesFinglish = [
+    'Shanbeh',
+    'Yekshanbeh',
+    'Doshanbeh',
+    'Seshanbeh',
+    'Chaharshanbeh',
+    'Panjshanbeh',
+    'Jomeh',
+  ];
+
   /// Jalali month name
   @override
   String get mN {
     return _monthNames[date.month - 1];
+  }
+
+  /// Jalali month names in Finglish
+  String get mNFn {
+    return _monthNamesFinglish[date.month - 1];
   }
 
   /// Jalali month name in Afghanistan
@@ -68,6 +100,11 @@ class JalaliFormatter extends DateFormatter {
   @override
   String get wN {
     return _weekDayNames[date.weekDay - 1];
+  }
+
+  /// Jalali week day name in Finglish
+  String get wNFn {
+    return _weekDayNamesFinglish[date.weekDay - 1];
   }
 
   /// Default date Seperator = `/`
